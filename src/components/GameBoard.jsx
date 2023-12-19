661b07d4642cb486e5fe210d5e66406dc419f27f
Balazs <Hungary3176>
2023-12-19 13:59:05 +0200

    random words generate

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,12 +1,28 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./GameBoard.css"
+import * as pokemons from "../pokemons.json";
 import Table from 'react-bootstrap/Table';
 import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
 
 function GameBoard() {
 
-    const [currentSelectedWord, setCurrentSelectedWord] = useState(""); 
+    const [wordsToFind, setWordsToFind] = useState([]);
+    let currentSelectedWord = "";
+    const pokemonsList = pokemons["default"];
+    const min = Math.ceil(0);
+    const max = Math.floor(51);
+
+    useEffect(() => {
+        for(let i = 0; i < 2; i++) {
+            let currID = Math.floor(Math.random() * (max - min) + min);
+            let name = (pokemonsList[currID]["name"]).toUpperCase();
+            let newWord = {"word" : name, "isFounded" : false};
+            setWordsToFind((words) => [...words, newWord]);
+        }
+      }, []);
+
+    
 
     const board = [
         ["A", "C", "H", "A", "R", "I", "Z", "A", "R", "D"],
@@ -21,25 +37,20 @@ function GameBoard() {
         ["A", "B", "C", "D", "P", "I", "D", "G", "E", "Y"]
     ];
 
-    const wordsToFind = ["CHARIZARD", "PIDGEY", "PIKACHU"];
-
     function cellClicked(e, key) {
         console.log(key);
-        setCurrentSelectedWord(`${currentSelectedWord}${e.target.dataset.value}`);
+        currentSelectedWord = `${currentSelectedWord}${e.target.dataset.value}`;
         e.target.style.color = "green";
-        //e.target.style.textDecoration="line-through";
-
     }
 
     function wordSubmit() {
-        if(wordsToFind.includes(currentSelectedWord)) {
-            alert("Nice! Match Founded!");
-            setCurrentSelectedWord("");
-        }
-        else {
-            alert("Wrong Word!");
-            setCurrentSelectedWord("");
-        }
+        wordsToFind.forEach((item) => {
+            if(item["word"] == currentSelectedWord) {
+                alert("Nice! Match Founded!");
+                item["isFounded"] = true;
+                currentSelectedWord = "";
+            }
+        })
     }
 
     return (
@@ -61,7 +72,8 @@ function GameBoard() {
         </Table>
         <Button onClick={() => {wordSubmit()}}>Submit</Button>
         {wordsToFind.map((word) => {
-            return (<p>{word}</p>)
+            if(!word["isFounded"]){return (<p>{word["word"]}</p>)}
+            else{return (<p className="line-through">{word["word"]}</p>)}
         })}
         <h1>{currentSelectedWord}</h1>
         </Container>
